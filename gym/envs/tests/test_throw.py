a3437b2a58cd78b25b0a66af154124cc06317901
Patrick Matthäi <pmd.matthaei@gmail.com>
2019-09-20 15:03:21 +0200

    - Removed bugs

--- gym/envs/tests/test_throw.py
+++ gym/envs/tests/test_throw.py
@@ -11,14 +11,9 @@ for i in range(episodes):
     obs = env.reset()
 
     for j in range(episode_length):
-        start = time.time()
-
         obs, reward, done, info = env.step(env.action_space.sample())
 
         env.render()
-
-        end = time.time()
-        print("Time:", end - start)
 
         if done:  # Ball dropped
             break
